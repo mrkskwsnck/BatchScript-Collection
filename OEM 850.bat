efb195b267ece2de68a86bca0eff6fd1c8dfd001
Markus Kwaśnicki <marek@kwasniccy.eu>
2015-04-29 22:39:48 +0200

    Example of western european encoding for Batch files

---
+++ OEM 850.bat
@@ -0,0 +1,3 @@
+REM Das ist eine Windows-Batch-Beispieldatei. Diese soll den Umgang mit der 
+REM korrekten Zeichensatzkodierung (OEM 850) demonstrieren. Nachfolgend sind die 
+REM deutschen Sonderzeichen aufgefhrt:  „ ™ ” š  á
